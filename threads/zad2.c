8812d416b78d0f972917e0592a109450445d7500
andrija-tosic <andonnet@gmail.com>
2021-12-12 11:41:22 -0500

    delete threads

--- threads/zad2.c
+++
@@ -1,9 +0,0 @@
-/*
-Koriscenjem programskog jezika C kreirati program koji se deli u dva Linux procesa koji generisu slucajne
-celobrojne vrednosti i upisuju ih u datoteku ciji se naziv prosledjuje kao argument komandne linije glavnog procesa.
-Prvi proces generise niz od tri slucajne pozitivne celobrojne vrednosti i upisuje ih u datoteku.
-Nakon toga drugi proces generise dve slucajne negativne celobrojne vrendosti i upisuje ih u istu datoteku.
-Postupak se ponavlja N puta (naizmenicno prvi pa drugi proces).
-Vrednost N se takodje prosledjuje kao argument komandne linije. Sinhronizovati rad procesa koriscenjem semafora.
-*/
-
