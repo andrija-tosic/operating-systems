f8c566a92d3b8b8bda91ef36c4158eaa80284a68
andrija-tosic <andonnet@gmail.com>
2021-12-12 11:37:30 -0500

    zad1 system v processes

---
+++ processes_and_threads/zad4.c
@@ -0,0 +1,7 @@
+/*
+Koriscenjem programs kog Jezika C naplsati Linux program threadovi u kome dve niti
+u tekstualnu datoteku upisuju redom cele brojeve ad 1 do 20,
+pri čemu je 5vaki broj u zasebnom redu.
+Prva nit upisuje samo neparne, a druga nit samo parne brojeve.
+sinhronizacijom niti obezbediti da se brojevi upisuju u datoteku pravilnim redosledam, 12 3 45 itd.
+*/
